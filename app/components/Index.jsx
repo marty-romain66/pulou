4795a91431552003d9e39923889819839fc376eb
Marty Romain <102585723+marty-romain66@users.noreply.github.com>
2023-07-21 23:47:00 +0200

    Add files via upload

--- app/components/Index.jsx
+++ app/components/Index.jsx
@@ -6,7 +6,7 @@ export default function Index() {
       <h1 className='mt-3 heading2  title  mx-auto text-6xl lg:text-9xl sm:mt-4'>Les Aventures de Pulou</h1>
     </div>
       <video className="h-full w-full rounded-lg" controls>
-      <source src="/pulou2.mp4" type="video/mp4" />
+      <source src="https://res.cloudinary.com/dcl00yipc/video/upload/v1689975757/pulou2_xxdroq.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video></>
   )
